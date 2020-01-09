f89ee74b6dbb2abf58d327b372822ea963b7a656
Sony AK <sony@lifull-media.id>
2020-01-09 11:17:48 +0700

    add cases

--- select.php
+++ select.php
@@ -10,6 +10,7 @@
 		while($row = mysqli_fetch_assoc($result)) {
 			// DO SOMETHING HERE WITH $row
 			// e.g. var_dump($row);
+			
 			echo $row['name'] . "\n";
 		}
 
@@ -18,7 +19,10 @@
 
 	mysql_close($dbConn);
 
+	//////////////////////////////////////////////////////////
+
 	// select data from a table (non-loop)
+	// $sql = 'SELECT * FROM `table_name`';
 	// if($result = mysqli_query($dbConn, $sql)) {
 	// 	if(mysqli_num_rows($result) > 0) {
 	// 		mysqli_data_seek($result, 0);
@@ -30,5 +34,60 @@
 	// 		mysqli_free_result($result);
 	// 	}
 	// }
+
+	// mysqli_close($dbConn);
+
+	//////////////////////////////////////////////////////////
+
+	// select data from a table using prepared statement (loop)
+	// $preparedStatement = mysqli_prepare($dbConn, 'SELECT * FROM `table_name` WHERE `id` = ? AND `name` = ?');
+	
+	// mysqli_stmt_bind_param($preparedStatement, 'is', $id, $name);
+	
+	// $executeStatus = mysqli_stmt_execute($preparedStatement);
+	// if($executeStatus) {
+	// 	$result = mysqli_stmt_get_result($preparedStatement);
+			
+	// 	while($row = mysqli_fetch_assoc($result)) {
+	// 		// DO SOMETHING HERE WITH $row
+	// 		// e.g. var_dump($row);
+
+	// 		echo $row['name'] . "\n";
+	// 	}
+		
+	// 	mysqli_free_result($result);
+	// }
+
+	// mysqli_stmt_close($preparedStatement);
+
+	// mysqli_close($dbConn);
+
+	//////////////////////////////////////////////////////////
+
+	// select data from a table using prepared statement (non-loop)
+	// $preparedStatement = mysqli_prepare($dbConn, 'SELECT * FROM `table_name` WHERE `id` = ? AND `name` = ?');
+	
+	// mysqli_stmt_bind_param($preparedStatement, 'is', $id, $name);
+	
+	// $executeStatus = mysqli_stmt_execute($preparedStatement);
+	// if($executeStatus) {
+	// 	$result = mysqli_stmt_get_result($preparedStatement);
+			
+	// 	if($result = mysqli_query($dbConn, $sql)) {
+	// 		if(mysqli_num_rows($result) > 0) {
+	// 			mysqli_data_seek($result, 0);
+		        
+	// 			$row = mysqli_fetch_assoc($result);
+
+	// 			$name = $row['name'];
+		        
+	// 			mysqli_free_result($result);
+	// 		}
+	// 	}
+		
+	// 	mysqli_free_result($result);
+	// }
+
+	// mysqli_stmt_close($preparedStatement);
 
 	// mysqli_close($dbConn);
