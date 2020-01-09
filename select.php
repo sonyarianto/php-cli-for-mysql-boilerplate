01aad8825922f294646c070b4b33c368c6b465d8
Sony AK <sony@sony-ak.com>
2020-01-09 10:36:26 +0700

    Create select.php

---
+++ select.php
@@ -0,0 +1,15 @@
+<?php
+	$dbConn = mysqli_connect('HOST', 'USER', 'PASSWORD', 'DATABASE_NAME');
+
+	mysqli_set_charset($dbConn, 'utf8mb4');
+  	mysqli_query($dbConn, "SET NAMES 'utf8mb4'");
+
+  	// select to a table
+  	$sql = 'SELECT * FROM `table_name`';
+  	if($result = mysqli_query($dbConn, $sql)) {
+  		while($row = mysqli_fetch_assoc($result)) {
+        	// DO SOMETHING HERE WITH $row
+        	// e.g. var_dump($row);
+        	echo $row['name'] . "\n";
+      	}
+  	}
